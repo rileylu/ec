538a02e931ab45bb86ed2c5e05a440a036e589e9
Riley <rileylu@yeah.net>
2018-06-22 20:15:46 +0800

    init
    commit

---
+++ domain/model/expressconfig.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lmz on 6/22/18.
+//
+
+#include "expressconfig.h"
---
+++ domain/model/expressconfig.h
@@ -0,0 +1,25 @@
+//
+// Created by Lmz on 6/22/18.
+//
+
+#ifndef EC_EXPRESSCONFIG_H
+#define EC_EXPRESSCONFIG_H
+
+#include <Poco/Types.h>
+#include <Poco/Nullable.h>
+#include <string>
+
+namespace domain {
+    namespace model {
+
+        class ExpressConfig {
+        public:
+        private:
+            Poco::UInt64 _id;
+
+        };
+    }
+}
+
+
+#endif //EC_EXPRESSCONFIG_H
